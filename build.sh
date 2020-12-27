670f4facb8e9282a1d9ed62b06b14cffe5b8ce5c
Markus Chmelar <mexx@innovaptor.com>
2020-12-27 14:08:21 +0100

    Add Build script

---
+++ build.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+
+# exit on error
+set -o errexit
+
+# Initial setup
+mix deps.get --only prod
+MIX_ENV=prod mix compile
+
+# Compile assets
+# (No assets currently)
+# npm install --prefix ./assets
+# npm run deploy --prefix ./assets
+# mix phx.digest
+
+# Build the release and overwrite the existing release directory
+MIX_ENV=prod mix release --overwrite
+
+# Perform any migrations necessary 
+# (No migrations currently)
+# _build/prod/rel/myApp/bin/myApp eval "MyApp.Release.migrate"
