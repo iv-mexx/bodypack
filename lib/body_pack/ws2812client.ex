28749cbd2409233130382d1cca6fffae9a3780c3
Markus Chmelar <mexx@innovaptor.com>
2021-08-08 23:57:30 +0200

    Update Programs

--- lib/body_pack/ws2812client.ex
+++ lib/body_pack/ws2812client.ex
@@ -11,9 +11,9 @@ defmodule BodyPack.Ws2812Client do
   setup 1,32,3;
   init;
   brightness 1,15;
-  do; fill 1,ff0000; render; delay 25; fill 1,000000; render; delay 150; loop 3;
-  fill 1,ff0000;
-  brightness 1,0;
+  do; fill 1,0000ff; render; delay 25; fill 1,000000; render; delay 150; loop 5;
+  fill 1,000080;
+  brightness 1,5;
   render;
   """
 
@@ -45,6 +45,10 @@ defmodule BodyPack.Ws2812Client do
 
     :ok = :gen_tcp.send(socket, message)
 
+    {:noreply, [], socket}
+  end
+
+  def handle_info({:tcp, port, msg}, socket) do
     {:noreply, [], socket}
   end
 end
