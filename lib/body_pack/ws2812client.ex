0ebcb87cc3b3908b92a94fea90f79c32dd8a1ee2
Markus Chmelar <mexx@innovaptor.com>
2023-05-08 10:25:34 +0200

    Re-Enable startup sequence

--- lib/body_pack/ws2812client.ex
+++ lib/body_pack/ws2812client.ex
@@ -7,7 +7,14 @@ defmodule BodyPack.Ws2812Client do
   @default_ws2812srv_port 9999
 
   @startup_sequence """
-
+  reset;
+  setup 1,32,3;
+  init;
+  brightness 1,15;
+  do; fill 1,0000ff; render; delay 25; fill 1,000000; render; delay 150; loop 5;
+  fill 1,000080;
+  brightness 1,5;
+  render;
   """
 
   def start_link(options \\ []) do
